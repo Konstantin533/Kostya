6fdcad089824730d57fff4952b68bc3d862e7e6e
Konstantin533 <petya.didi@mail.ru>
2023-02-01 17:23:14 +0300

    init

---
+++ Listing01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+namespace Listing01
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name;
+            Console.Write("Введите ваше имя: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Очень приятно, " + name);
+        }
+    }
+}
